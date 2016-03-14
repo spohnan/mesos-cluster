d160579a25a3f7e055dde4c65a23d311396d3a15
Andy Spohn <Andy@AndySpohn.com>
2016-03-14 15:28:19 -0400

    Continued tweaks to logging

--- files/marathon.sh
+++ files/marathon.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
+#
+# NOTE: Run the deploy action first to generate the config file from the template
+#
+# Usage: ./marathon.sh
+#           --marathon      HOSTNAME_OF_MARATHON_NODE
+#           --type          [groups (default)|apps]
+#           --socks         SOCKS_PROXY_PORT
+#           --config        CONFIG_FILE
+#
+# Example:
+#   1.) Create the deployment scripts: ./mesos.sh --prefix t8 --vars my-env.yml --action deploy
+#   2.) Deploy:  deploy/marathon.sh --marathon t8-mesos-master-76301b --socks 5556 --config deploy/infrastructure.json
+#
 
-MARATHON_HOST="${1-localhost}"
+# Parse arguments
+declare -A ARGS
+# Defaults
+ARGS[marathon]="localhost"
+ARGS[type]="groups"
+while [ $# -gt 0 ]; do
+	# Trim the first two chars off of the arg name ex: --foo
+	case "$1" in
+		*) NAME="${1:2}"; shift; ARGS[$NAME]="${1-true}" ;;
+	esac
+	shift
+done
 
-marathon_launch() {
-  curl -X POST --socks5-hostname localhost "http://${MARATHON_HOST}:8080/v2/apps" -d @$2 -H 'Content-type: application/json'
-}
+if [ ! -z "${ARGS[socks]}" ]; then
+    SOCKS_PROXY="--socks5-hostname localhost:${ARGS[socks]}"
+fi
 
-marathon_launch_group() {
-  curl -X POST --socks5-hostname localhost "http://${MARATHON_HOST}:8080/v2/groups" -d @$2 -H 'Content-type: application/json'
-}
-
-alias ml='marathon_launch'
-alias mlg='marathon_launch_group'
+curl -X POST $SOCKS_PROXY "http://${ARGS[marathon]}:8080/v2/${ARGS[type]}" \
+    -d "@${ARGS[config]}" -H 'Content-type: application/json'
